cb3492f61d05b835858c5585a828c24d1e106b98
poymanov <n.poymanov@gmail.com>
2019-09-18 20:42:58 +0300

    Added endpoint to getting customer by id

--- src/main/java/com/learning/simplecrm/rest/CustomerRestController.java
+++ src/main/java/com/learning/simplecrm/rest/CustomerRestController.java
@@ -4,6 +4,7 @@ import com.learning.simplecrm.entity.Customer;
 import com.learning.simplecrm.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,5 +19,10 @@ public class CustomerRestController {
     @GetMapping("/customers")
     public List<Customer> getCustomers() {
         return customerService.getCustomers();
+    }
+
+    @GetMapping("/customers/{id}")
+    public Customer getCustomer(@PathVariable int id) {
+        return customerService.getCustomer(id);
     }
 }
