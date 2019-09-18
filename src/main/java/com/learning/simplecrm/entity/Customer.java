c67eb6c5d7ba52d49476463a284706387cbf0230
poymanov <n.poymanov@gmail.com>
2019-09-18 20:33:25 +0300

    Added endpoint to getting list of customers

--- src/main/java/com/learning/simplecrm/entity/Customer.java
+++ src/main/java/com/learning/simplecrm/entity/Customer.java
@@ -8,7 +8,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="customer")
+@Table(name="customers")
 public class Customer {
 
 	@Id
---
+++ src/main/java/com/learning/simplecrm/rest/CustomerRestController.java
@@ -0,0 +1,22 @@
+package com.learning.simplecrm.rest;
+
+import com.learning.simplecrm.entity.Customer;
+import com.learning.simplecrm.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class CustomerRestController {
+    @Autowired
+    private CustomerService customerService;
+
+    @GetMapping("/customers")
+    public List<Customer> getCustomers() {
+        return customerService.getCustomers();
+    }
+}
